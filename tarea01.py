3117860b4b7f8e0bce2ce948fbadfde8d824c956
Nacho Valencia <nacho@MacBook-Pro-de-Nacho.local>
2017-03-16 12:06:17 -0300

    Avance tarea 1

--- tarea01.py
+++ tarea01.py
@@ -11,38 +11,70 @@ with open("recursos.csv") as f:
     #print(dicc[2])
 
 class usuario:
+
     def __init__(self,id = "",nombre = "",contrasena = "",recurso_id="",**kwargs):
         self.id = id
         self.nombre = nombre
         self.contrasena = contrasena
         self.recurso_id= recurso_id
 
-class Menu:
+class Archivos:
+
+     def __init__(self):
+         pass
+
+     def cargar_usuarios(self):
+         self.lista_usuarios = []
+         primera_linea = []
+         llaves_columnas = []
+         with open("usuarios.csv")as archivo_usuarios:
+             contador = 0
+             for linea in archivo_usuarios:
+                 if contador < 1:
+                     primera_linea = (linea.strip("\n").split(","))
+                     contador += 1
+                     for columna in primera_linea:
+                         llaves_columnas.append(columna.split(":")[0])
+                 else:
+                     id_usuario = linea.strip("\n").split(",")[llaves_columnas.index("id")]
+                     contrasena_usuario = linea.strip("\n").split(",")[llaves_columnas.index("contraseña")]
+                     nombre_usuario = linea.strip("\n").split(",")[llaves_columnas.index("nombre")]
+                     recurso_usuario = linea.strip("\n").split(",")[llaves_columnas.index("recurso_id")]
+                     usuario1 = usuario(id=id_usuario, nombre=nombre_usuario, contrasena=contrasena_usuario,
+                                        recurso_id=recurso_usuario)
+                     self.lista_usuarios.append(usuario1)
+         return self.lista_usuarios
+
+class SuperLuchin:
 
     def __init__(self):
         self.lista_usuarios = []
+        self.usuario_activo = []
+        self.fecha = ""
+        self.hora = ""
+        self.archivos = Archivos()
+        print("---- Bienvenido al Software SuperLuchin -----")
 
     def iniciar_sesion(self):
-        primera_linea = []
-        llaves_columnas = []
-        with open("usuarios.csv")as archivo_usuarios:
-            contador = 0
-            for linea in archivo_usuarios:
-                if contador < 1:
-                    primera_linea=(linea.strip("\n").split(","))
-                    contador += 1
-                    for columna in primera_linea:
-                        llaves_columnas.append(columna.split(":")[0])
-                    print(llaves_columnas)
-                else:
-                    id_usuario = linea.strip("\n").split(",")[llaves_columnas.index("id")]
-                    contrasena_usuario = linea.strip("\n").split(",")[llaves_columnas.index("contraseña")]
-                    nombre_usuario = linea.strip("\n").split(",")[llaves_columnas.index("nombre")]
-                    recurso_usuario = linea.strip("\n").split(",")[llaves_columnas.index("recurso_id")]
-                    usuario1 = usuario(id = id_usuario,nombre=nombre_usuario,contrasena=contrasena_usuario,recurso_id=recurso_usuario)
-                    self.lista_usuarios.append(usuario1)
-            print(self.lista_usuarios[200].nombre)
+        self.lista_usuarios = self.archivos.cargar_usuarios()
+        identificador = False
+        while identificador == False:
+            usuario_ingresado = input("ingrese usuario: ")
+            clave_ingresada = input("ingrese contraseña: ")
+            for usuarios_existentes in self.lista_usuarios:
+               if usuario_ingresado == usuarios_existentes.nombre and clave_ingresada == usuarios_existentes.contrasena:
+                   identificador = True
+                   self.usuario_activo = usuarios_existentes
+                   break
+               else:
+                  identificador = False
+            if identificador == False:
+                print("clave o usuario incorrecto")
+        print("Estas dentro")
+
+    def cambiar_fecha_hora(self):
+        pass
 
 
-ejecutar = Menu()
+ejecutar = SuperLuchin()
 ejecutar.iniciar_sesion()
